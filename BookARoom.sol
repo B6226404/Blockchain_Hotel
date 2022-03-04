348331d1ce10b2070f0df8d163a16c00b1563bfc
B6226404 <b6226404@g.sut.ac.th>
2022-03-05 01:32:27 +0700

    Update V.2

--- BookARoom.sol
+++ BookARoom.sol
@@ -37,15 +37,68 @@ contract BookARoom {
         return;
     }
 
-    //---check if msg.value != 0.001 ether---
-    if (msg.value != 0.001 ether) {
-        //---fire the event---
-      emit ReserveError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.001 ether");
-        //---refund back to the sender---
-      payable(msg.sender).transfer(msg.value);
-        //---exit the function---
-        return;
+    if (hashing(name) == hashing("Room1")) {
+        //---check if msg.value != 0.001 ether---
+      if (msg.value != 0.001 ether) {
+          //---fire the event---
+        emit ReserveError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.001 ether");
+          //---refund back to the sender---
+        payable(msg.sender).transfer(msg.value);
+          //---exit the function---
+          return;
+      }
     }
+    else if (hashing(name) == hashing("Room2")) {
+      if (msg.value != 0.002 ether) {
+          //---fire the event---
+        emit ReserveError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.002 ether");
+          //---refund back to the sender---
+        payable(msg.sender).transfer(msg.value);
+          //---exit the function---
+          return;
+      }
+    }
+    else if (hashing(name) == hashing("Room3")) {
+      if (msg.value != 0.003 ether) {
+          //---fire the event---
+        emit ReserveError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.003 ether");
+          //---refund back to the sender---
+        payable(msg.sender).transfer(msg.value);
+          //---exit the function---
+          return;
+      }
+    }
+    else if (hashing(name) == hashing("Room4")) {
+      if (msg.value != 0.004 ether) {
+          //---fire the event---
+        emit ReserveError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.004 ether");
+          //---refund back to the sender---
+        payable(msg.sender).transfer(msg.value);
+          //---exit the function---
+          return;
+      }
+    }
+    else if (hashing(name) == hashing("Room5")) {
+      if (msg.value != 0.005 ether) {
+          //---fire the event---
+        emit ReserveError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.005 ether");
+          //---refund back to the sender---
+        payable(msg.sender).transfer(msg.value);
+          //---exit the function---
+          return;
+      }
+    }
+    else if (hashing(name) == hashing("Room6")) {
+      if (msg.value != 0.006 ether) {
+          //---fire the event---
+        emit ReserveError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.006 ether");
+          //---refund back to the sender---
+        payable(msg.sender).transfer(msg.value);
+          //---exit the function---
+          return;
+      }
+    }
+    
  
     recordProof(hashing(name));
     
